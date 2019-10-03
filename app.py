0925566476efbb0d912a7228b54336d67046cdbe
dwb217 <davidwbender@gmail.com>
2019-10-02 21:07:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,35 @@ tabtitle='2020'
 mytitle = 'Chart'
 dem_candidates=('Warren', 'Biden', 'Sanders', 'Buttigieg')
 poll_dates=['July', 'August', 'September', 'October']
-poll_data=[(20,23,33,45),(22,29,27,26),(13,15,16,10),(12,10,9,7)]
+poll_data=[[20,23,33,45],[22,29,27,26],[13,15,16,10],[12,10,9,7]]
 color_ew='#fc8403'
 color_jb='#0317fc'
 color_bs='#9013fc'
 color_pb='#05F935'
 
 trace0 = go.Scatter(
-    x = poll_dates[0],
+    x = poll_dates,
     y = poll_data[0],
     mode = 'lines',
     marker = {'color': color_ew},
     name = dem_candidates[0]
 )
 trace1 = go.Scatter(
-    x = poll_dates[1],
+    x = poll_dates,
     y = poll_data[1],
     mode = 'lines',
     marker = {'color': color_jp},
     name = dem_candidates[1]
 )
 trace2 = go.Scatter(
-    x = poll_dates[2],
+    x = poll_dates,
     y = poll_data[2],
     mode = 'lines',
     marker = {'color': color_bs},
     name = dem_candidates[2]
 )
 trace3 = go.Scatter(
-    x = poll_dates[3],
+    x = poll_dates,
     y = poll_data[3],
     mode = 'lines',
     marker = {'color': color_pb},
